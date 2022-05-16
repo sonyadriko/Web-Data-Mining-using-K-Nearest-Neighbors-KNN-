cc8ac3a36b47113b10567b54e7cc3dfaa48c95d3
sonyadriko <sonyadiadriko@gmail.com>
2022-05-17 06:31:34 +0700

    update login & remove logo sbadmin

--- sidebar.php
+++ sidebar.php
@@ -2,15 +2,15 @@
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
+           <!--  <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
                 <div class="sidebar-brand-icon rotate-n-15">
                     <i class="fas fa-laugh-wink"></i>
                 </div>
                 <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
-            </a>
+            </a> -->
 
             <!-- Divider -->
-            <hr class="sidebar-divider my-0">
+            <!-- <hr class="sidebar-divider my-0"> -->
 
             <!-- Nav Item - Dashboard -->
             <li class="nav-item active">
@@ -63,4 +63,4 @@
         
 
         </ul>
-        <!-- End of Sidebar -->
+        <!-- End of Sidebar
